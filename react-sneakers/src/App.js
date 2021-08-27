b4581674696d13e3bc4a991c0dfb93d6f4c759b1
atlaua18 <nastyaopen@yandex.ru>
2021-08-27 22:12:33 +0300

    add favorites

--- react-sneakers/src/App.js
+++ react-sneakers/src/App.js
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
-import { Card } from "./components/Card/Card";
+// import { Card } from "./components/Card/Card";
 import { DrawerCart } from "./components/DrawerCart/DrawerCart";
 import { Header } from "./components/Header/Header";
 import axios from "axios";
+import { Home } from "./pages/Home";
+import { Route } from "react-router-dom";
+import { Favorites } from "./pages/Favorites";
 
 function App() {
     const [items, setItems] = useState([]);
     const [cartItems, setCartItems] = useState([]);
+    const [favorites, setFavorites] = useState([]);
     const [searchValue, setSearchValue] = useState("");
     const [cartOpened, setCartOpened] = React.useState(false);
 
@@ -23,15 +27,36 @@ function App() {
             .get("https://610c092a66dd8f0017b76c0b.mockapi.io/items")
             .then((res) => setItems(res.data));
 
-            axios
+        axios
             .get("https://610c092a66dd8f0017b76c0b.mockapi.io/cart")
             .then((res) => setCartItems(res.data));
+
+        axios
+            .get("https://610c092a66dd8f0017b76c0b.mockapi.io/favorites")
+            .then((res) => setFavorites(res.data));
     }, []);
 
     const onAddToCart = (obj) => {
-        axios
-        .post("https://610c092a66dd8f0017b76c0b.mockapi.io/cart", obj);
+        axios.post("https://610c092a66dd8f0017b76c0b.mockapi.io/cart", obj);
         setCartItems((prev) => [...prev, obj]);
+    };
+
+    const onAddFavorite = async (obj) => {
+        try {
+            if (favorites.find((favObj) => favObj.id === obj.id)) {
+                axios.delete(
+                    `https://610c092a66dd8f0017b76c0b.mockapi.io/favorites/${obj.id}`
+                );
+            } else {
+                const { data } = await axios.post(
+                    "https://610c092a66dd8f0017b76c0b.mockapi.io/favorites",
+                    obj
+                );
+                setFavorites((prev) => [...prev, data]);
+            }
+        } catch (error) {
+            alert('Не удалось добавить в избранное');
+        }
     };
 
     const onChangeSearchInput = (event) => {
@@ -40,10 +65,9 @@ function App() {
     };
 
     const onRemoveItem = (id) => {
-        axios
-        .delete(`https://610c092a66dd8f0017b76c0b.mockapi.io/cart/${id}`);
-        setCartItems((prev) => prev.filter(item => item.id !== id)); // скрыть элементы корзины с помощью react
-    }
+        axios.delete(`https://610c092a66dd8f0017b76c0b.mockapi.io/cart/${id}`);
+        setCartItems((prev) => prev.filter((item) => item.id !== id)); // скрыть элементы корзины с помощью react
+    };
 
     return (
         <div className="wrapper clear">
@@ -54,67 +78,23 @@ function App() {
                     onRemove={onRemoveItem}
                 />
             )}
+
             <Header onClickCart={() => setCartOpened(true)} />
 
-            <div className="content p-40">
-                <div className="d-flex justify-between align-center mb-40">
-                    <h1>
-                        {searchValue
-                            ? `Поиск по запросу: "${searchValue}"`
-                            : "Все кроссовки"}
-                    </h1>
-                    <div className="search-block d-flex align-center">
-                        <svg
-                            width="16"
-                            height="16"
-                            viewBox="0 0 16 16"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                        >
-                            <path
-                                d="M15.25 15.25L11.8855 11.8795L15.25 15.25ZM13.75 7.375C13.75 9.06576 13.0784 10.6873 11.8828 11.8828C10.6873 13.0784 9.06576 13.75 7.375 13.75C5.68424 13.75 4.06274 13.0784 2.86719 11.8828C1.67165 10.6873 1 9.06576 1 7.375C1 5.68424 1.67165 4.06274 2.86719 2.86719C4.06274 1.67165 5.68424 1 7.375 1C9.06576 1 10.6873 1.67165 11.8828 2.86719C13.0784 4.06274 13.75 5.68424 13.75 7.375V7.375Z"
-                                stroke="#E4E4E4"
-                                stroke-width="2"
-                                stroke-linecap="round"
-                            />
-                        </svg>
-                        {searchValue && (
-                            <img
-                                onClick={() => setSearchValue("")}
-                                className="clear cu-p"
-                                src="/img/btn-remove.svg"
-                                alt="Close"
-                            />
-                        )}
-                        <input
-                            onChange={onChangeSearchInput}
-                            value={searchValue}
-                            placeholder="Поиск..."
-                        />
-                    </div>
-                </div>
+            <Route path="/" exact>
+                <Home
+                    items={items}
+                    searchValue={searchValue}
+                    setSearchValue={setSearchValue}
+                    onChangeSearchInput={onChangeSearchInput}
+                    onAddFavorite={onAddFavorite}
+                    onAddToCart={onAddToCart}
+                />
+            </Route>
 
-                <div className="d-flex flex-wrap">
-                    {items
-                        .filter((item) =>
-                            item.name
-                                .toLowerCase()
-                                .includes(searchValue.toLowerCase())
-                        )
-                        .map((item, index) => (
-                            <Card
-                                key={index}
-                                title={item.name}
-                                price={item.price}
-                                img={item.img}
-                                onClickFavorite={() =>
-                                    console.log("Добавили в закладки")
-                                }
-                                onClickBtnPlus={(obj) => onAddToCart(obj)}
-                            />
-                        ))}
-                </div>
-            </div>
+            <Route path="/favorites" exact>
+                <Favorites items={favorites} onAddFavorite={onAddFavorite} />
+            </Route>
         </div>
     );
 }
--- react-sneakers/src/components/Card/Card.js
+++ react-sneakers/src/components/Card/Card.js
@@ -1,34 +1,51 @@
 import React from "react";
 import styles from "./card.module.scss";
 
-export function Card({title, img, price, onClickFavorite, onClickBtnPlus}) {
-    // const onClickBtn = () => {
-    //     alert('sdfgsdg')
-    // }
+export function Card({
+    id,
+    name,
+    img,
+    price,
+    onFavorite,
+    onClickBtnPlus,
+    favorited = false,
+}) {
     const [isAdded, setIsAdded] = React.useState(false);
+    const [isFavorite, setIsFavorite] = React.useState(favorited);
 
     const onClickPlus = () => {
-        onClickBtnPlus({title, img, price});
+        onClickBtnPlus({ name, img, price });
         setIsAdded(!isAdded);
-    }
+    };
+
+    const onClickFavorite = () => {
+        onFavorite({ id, name, img, price });
+        setIsFavorite(!isFavorite);
+    };
 
     return (
         <div className={styles.card}>
             <div className={styles.favorite} onClick={onClickFavorite}>
-                <img src="/img/heart-unliked.svg" alt="Unliked" />
+                <img
+                    src={
+                        isFavorite
+                            ? "/img/heart-liked.svg"
+                            : "/img/heart-unliked.svg"
+                    }
+                    alt="Favorite"
+                />
             </div>
             <img width={133} height={112} src={img} alt="" />
-            <h5>{title}</h5>
+            <h5>{name}</h5>
             <div className="d-flex justify-between  align-center">
                 <div className="d-flex flex-column">
                     <span>Цена: </span>
                     <b>{price} руб.</b>
                 </div>
-                {/* onClick={() => onClickBtn()} */}
                 <img
                     className={styles.plus}
                     onClick={onClickPlus}
-                    src = {isAdded ? "/img/btn-checked.svg" : "/img/btn-plus.svg"}
+                    src={isAdded ? "/img/btn-checked.svg" : "/img/btn-plus.svg"}
                     alt="Плюс"
                 />
             </div>
